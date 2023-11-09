d31772110d3b78118a0b499b908553c8e4b088a9
Dauniusha <daniilegiptsev@gmail.com>
2023-11-09 09:58:57 +0300

    feat: restructure search panel classes

---
+++ src/js/search-panel/filter.js
@@ -0,0 +1,180 @@
+import { getMonth, getYear } from "date-fns";
+
+// Need refactoring filter
+export class SearchPanelFilter {
+  searchPanel = null;
+
+  constructor(searchPanel) {
+    this.searchPanel = searchPanel;
+  }
+
+  getFormData() {
+    const formDataSchema = {
+      destinations: [],
+      dates: [],
+      ports: [],
+      adults: 0,
+      childrens: 0,
+      infants: 0,
+    };
+
+    const formData = new FormData(this.searchPanel.searchForm);
+    // Get all destindation
+    formDataSchema.destinations = formData.getAll('destinations').slice(0);
+    // Get all ports
+    formDataSchema.ports = formData.getAll('ports').slice(0);
+    // Get all dates
+    const activeDates = this.searchPanel.searchForm.querySelectorAll('button[data-date][data-active]');
+
+    if (activeDates.length) {
+      activeDates.forEach(date => {
+        formDataSchema.dates.push(date.getAttribute('data-date'));
+      })
+    }
+
+    return formDataSchema;
+  }
+
+  filterData() {
+    const formData = this.getFormData();
+    const filtered = {
+      destinations: {},
+      ports: {},
+      dates: {}
+    };
+    let filteredDestinations = [...this.searchPanel.allDestinations];
+    let filteredPorts = [...this.searchPanel.allPorts];
+    let filteredDates = [...this.searchPanel.allDates];
+
+    this.searchPanel.allVoyages.forEach(({ pkg }) => {
+      const date = `${getYear(pkg.vacation.from)}-${getMonth(pkg.vacation.from) + 1}`;
+      const port = pkg.location.from.code;
+      // Destinations
+      pkg.destinations.forEach(destination => {
+        if (!filtered.destinations[destination.key]) {
+          filtered.destinations[destination.key] = {};
+          filtered.destinations[destination.key].ports = new Set();
+          filtered.destinations[destination.key].dates = new Set();
+        }
+        filtered.destinations[destination.key].ports.add(port);
+        filtered.destinations[destination.key].dates.add(date);
+      })
+
+      // Ports
+      if (!filtered.ports[port]) {
+        filtered.ports[port] = {};
+        filtered.ports[port].destinations = new Set();
+        filtered.ports[port].dates = new Set();
+      }
+      pkg.destinations.forEach(destination => filtered.ports[port].destinations.add(destination.key));
+      filtered.ports[port].dates.add(date);
+
+      // Dates
+      if (!filtered.dates[date]) {
+        filtered.dates[date] = {};
+        filtered.dates[date].destinations = new Set();
+        filtered.dates[date].ports = new Set();
+      }
+      pkg.destinations.forEach(destination => filtered.dates[date].destinations.add(destination.key));
+      filtered.dates[date].ports.add(port);
+    })
+
+    if (formData.destinations.length) {
+      Object.keys(filtered.ports).forEach(key => {
+        if (![...filtered.ports[key].destinations].some(destination => formData.destinations.includes(destination))) {
+          filteredPorts = filteredPorts.filter(port => port !== key);
+        }
+      })
+      Object.keys(filtered.dates).forEach(key => {
+        if (![...filtered.dates[key].destinations].some(destination => formData.destinations.includes(destination))) {
+          filteredDates = filteredDates.filter(date => date !== key);
+        }
+      })
+    }
+
+    if (formData.ports.length) {
+      Object.keys(filtered.destinations).forEach(key => {
+        if (![...filtered.destinations[key].ports].some(port => formData.ports.includes(port))) {
+          filteredDestinations = filteredDestinations.filter(destination => destination !== key);
+        }
+      })
+      Object.keys(filtered.dates).forEach(key => {
+        if (![...filtered.dates[key].ports].some(port => formData.ports.includes(port))) {
+          filteredDates = filteredDates.filter(date => date !== key);
+        }
+      })
+    }
+
+    if (formData.dates.length) {
+      Object.keys(filtered.destinations).forEach(key => {
+        if (![...filtered.destinations[key].dates].some(date => formData.dates.includes(date))) {
+          filteredDestinations = filteredDestinations.filter(destination => destination !== key);
+        }
+      })
+      Object.keys(filtered.ports).forEach(key => {
+        if (![...filtered.ports[key].dates].some(date => formData.dates.includes(date))) {
+          filteredPorts = filteredPorts.filter(port => port !== key);
+        }
+      })
+    }
+
+    return {
+      destinations: filteredDestinations,
+      ports: filteredPorts,
+      dates: filteredDates
+    };
+  }
+
+  htmlChange(filterData) {
+    const destinationsInputs = this.searchPanel.searchForm.querySelectorAll('input[name="destinations"]');
+    const portsInputs = this.searchPanel.searchForm.querySelectorAll('input[name="ports"]');
+    const monthInputs = this.searchPanel.searchForm.querySelectorAll('[data-date]');
+
+    if (destinationsInputs.length) {
+      destinationsInputs.forEach(destinationInput => {
+        const destinationVal = destinationInput.value;
+        const parent = destinationInput.closest('[data-search-text-parent]');
+        if (!filterData.destinations.includes(destinationVal)) {
+          destinationInput.checked = false;
+          if (parent) {
+            parent.setAttribute('data-hidden', '');
+          }
+        } else {
+          parent.removeAttribute('data-hidden');
+        }
+      })
+    }
+
+    if (portsInputs.length) {
+      portsInputs.forEach(portInput => {
+        const portVal = portInput.value;
+        const parent = portInput.closest('[data-search-text-parent]');
+        if (!filterData.ports.includes(portVal)) {
+          portInput.checked = false;
+          if (parent) {
+            parent.setAttribute('data-hidden', '');
+          }
+        } else {
+          parent.removeAttribute('data-hidden');
+        }
+      })
+    }
+
+    if (monthInputs.length) {
+      monthInputs.forEach(monthItem => {
+        const dateVal = monthItem.getAttribute('data-date');
+        if (!filterData.dates.includes(dateVal)) {
+          monthItem.removeAttribute('data-active');
+          monthItem.setAttribute('data-disable', '');
+        } else {
+          monthItem.removeAttribute('data-disable');
+        }
+      })
+    }
+  }
+
+  process() {
+    const filterData = this.filterData();
+    this.htmlChange(filterData);
+  }
+}
---
+++ src/js/search-panel/renderer.js
@@ -0,0 +1,82 @@
+import { format } from "date-fns";
+import { enUS } from "date-fns/locale";
+
+export class SearchPanelRenderer {
+  constructor() { }
+
+  // Return searchCheckboxItem Node
+  createSearchCheckboxItem(checkboxName, checkboxValue, checkboxTextValue) {
+    const checkboxItem = document.createElement('div');
+    checkboxItem.className = 'search-form__checkbox-item';
+    checkboxItem.setAttribute('data-search-text-parent', '');
+    checkboxItem.innerHTML = `
+      <label class="search-form__label flex">
+        <input type="checkbox" name="${checkboxName}" value="${checkboxValue}">
+        <div class="checkbox-after"></div>
+        <span class="search-form__checkbox-text" data-search-text>${checkboxTextValue}</span>
+      </label>`;
+
+    return checkboxItem;
+  }
+
+  createMonthItem(date, monthName) {
+    const dateItem = document.createElement('div');
+    dateItem.className = 'search-form__date-item flex';
+    dateItem.innerHTML = `
+      <button type="button" class="search-form__date-month flex text-16" data-date="${date}">
+        ${monthName}
+      </button>`
+
+    return dateItem;
+  }
+
+  createDatesItems(dates, renderNode) {
+    const datesItems = [];
+    Object.keys(dates).forEach(key => {
+      const datesItem = document.createElement('div');
+      datesItem.className = 'search-form__dates-item flex';
+      datesItem.innerHTML = `
+          <div class="search-form__dates-item-title text-20 extrabold">
+            ${key}
+          </div>
+          <div class="search-form__date flex"></div>`;
+
+      const monthParent = datesItem.querySelector('.search-form__date');
+
+      dates[key].forEach(date => {
+        const dateObj = new Date(date);
+        const monthName = format(dateObj, 'LLL', { locale: enUS });
+        monthParent.appendChild(this.createMonthItem(date, monthName));
+      })
+
+      datesItems.push(datesItem);
+    })
+
+    datesItems.forEach(dateItem => renderNode.appendChild(dateItem));
+  }
+
+  render(renderData) {
+    const { destinations, ports, dates } = renderData;
+    // Destinations render
+    const destinationsWrapper = document.querySelector('[data-search-destinations]');
+    if (destinationsWrapper && destinations.length) {
+      destinations.forEach(destination => {
+        destinationsWrapper.appendChild(this.createSearchCheckboxItem('destinations', destination.key, destination.comment));
+      })
+    }
+
+    // Ports render
+    const portsWrapper = document.querySelector('[data-search-ports]')
+    if (portsWrapper && ports.length) {
+      ports.forEach(port => {
+        portsWrapper.appendChild(this.createSearchCheckboxItem('ports', port.code, port.name));
+      })
+    }
+
+    // Dates render
+    const datesWrapper = document.querySelector('[data-search-form-dates]');
+    if (datesWrapper && Object.keys(dates).length) {
+      this.createDatesItems(dates, datesWrapper);
+    }
+  }
+}
