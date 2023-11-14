258655f313d6d9360e8706eaf7f749306d0a2500
Slavalenets95 <V.Lenets@gpsolutions.com>
2023-11-14 21:07:08 +0300

    Ar & all-regions & contact-us design edits

--- src/js/_forms.js
+++ src/js/_forms.js
@@ -1,6 +1,10 @@
+import { wrap } from "gsap";
+
 class Forms {
   selectChangeColor() {
     const selects = document.querySelectorAll('[data-select]');
+    const selectWrappers = document.querySelectorAll('.select-wrapper');
+    const body = document.querySelector('body');
 
     if(selects.length) {
       selects.forEach(select => {
@@ -11,6 +15,29 @@ class Forms {
             evt.target.removeAttribute('data-selected');
           }
         })
+      })
+    }
+
+    if(selectWrappers.length) {
+      body.addEventListener('click', (evt) => {
+        const openedSelectWrapper = document.querySelectorAll('[data-select-open]');
+        
+        if(openedSelectWrapper.length && evt.target.tagName.toLowerCase() !== 'select') {
+          openedSelectWrapper.forEach(wrapper => wrapper.removeAttribute('data-select-open'));
+        }
+      })
+      selectWrappers.forEach(wrapper => {
+        const select = wrapper.querySelector('select');
+
+        if(select) {
+          select.addEventListener('click', () => {
+            if(wrapper.hasAttribute('data-select-open')) {
+              wrapper.removeAttribute('data-select-open');
+            } else {
+              wrapper.setAttribute('data-select-open', '');
+            }
+          })
+        }
       })
     }
   }
