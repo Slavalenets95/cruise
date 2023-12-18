f63eed5611a3f06f4ed96060c571bf33c7560804
Slavalenets95 <V.Lenets@gpsolutions.com>
2023-12-18 10:31:43 +0300

    Edits

--- src/js/_scroller.js
+++ src/js/_scroller.js
@@ -1,76 +1,76 @@
-import { gsap } from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
+// import { gsap } from "gsap";
+// import { ScrollTrigger } from "gsap/ScrollTrigger";
 
-gsap.registerPlugin(ScrollTrigger);
+// gsap.registerPlugin(ScrollTrigger);
 
-class Scroller {
-  scrollerNode = null;
-  scrollerScrollTrigger = null;
-  isMobile = screen.availWidth <= 768;
-  direction = 'ltr';
+// class Scroller {
+//   scrollerNode = null;
+//   scrollerScrollTrigger = null;
+//   isMobile = screen.availWidth <= 768;
+//   direction = 'ltr';
 
-  reInit() {
-    this.isMobile = screen.availWidth <= 768;
+//   reInit() {
+//     this.isMobile = screen.availWidth <= 768;
 
-    if(this.scrollerNode && this.scrollerScrollTrigger) {
-      if(this.isMobile) {
-        this.scrollerScrollTrigger.scrollTrigger.disable();
-      } else {
-        this.scrollerScrollTrigger.scrollTrigger.enable();
-      }
-    }
+//     if(this.scrollerNode && this.scrollerScrollTrigger) {
+//       if(this.isMobile) {
+//         this.scrollerScrollTrigger.scrollTrigger.disable();
+//       } else {
+//         this.scrollerScrollTrigger.scrollTrigger.enable();
+//       }
+//     }
 
-    if(!this.scrollerScrollTrigger && this.scrollerNode) {
-      this.gsapInit();
-    }
-  }
+//     if(!this.scrollerScrollTrigger && this.scrollerNode) {
+//       this.gsapInit();
+//     }
+//   }
 
-  getX() {
-    let x = this.scrollerNode.scrollWidth * -1;
+//   getX() {
+//     let x = this.scrollerNode.scrollWidth * -1;
 
-    if (this.direction === 'rtl') {
-      x = this.scrollerNode.scrollWidth - this.scrollerNode.offsetWidth;
-    }
+//     if (this.direction === 'rtl') {
+//       x = this.scrollerNode.scrollWidth - this.scrollerNode.offsetWidth;
+//     }
 
-    return x;
-  }
+//     return x;
+//   }
 
-  getXpercent() {
-    let xPercent = null;
+//   getXpercent() {
+//     let xPercent = null;
 
-    if (this.direction !== 'rtl') {
-      xPercent = 100;
-    }
+//     if (this.direction !== 'rtl') {
+//       xPercent = 100;
+//     }
 
-    return xPercent;
-  }
+//     return xPercent;
+//   }
 
-  gsapInit() {
-    this.scrollerNode = document.querySelector('.custom-scroll');
-    this.isMobile = screen.availWidth <= 768;
+//   gsapInit() {
+//     this.scrollerNode = document.querySelector('.custom-scroll');
+//     this.isMobile = screen.availWidth <= 768;
 
-    if (this.scrollerNode && !this.isMobile) {
-      this.direction = document.querySelector('html').getAttribute('dir');
-      this.scrollerScrollTrigger = gsap.to('.custom-scroll', {
-        x: () => this.getX(),
-        xPercent: this.getXpercent(),
-        scrollTrigger: {
-          trigger: '.custom-scroll',
-          start: 'center center',
-          direction: 'horizontal',
-          end: `+=${document.querySelector('.custom-scroll').scrollWidth}px`,
-          pin: 'body',
-          scrub: true,
-          invalidateOnRefresh: true,
-          ease: "power1.inOut"
-        },
-      })
-    }
-  }
+//     if (this.scrollerNode && !this.isMobile) {
+//       this.direction = document.querySelector('html').getAttribute('dir');
+//       this.scrollerScrollTrigger = gsap.to('.custom-scroll', {
+//         x: () => this.getX(),
+//         xPercent: this.getXpercent(),
+//         scrollTrigger: {
+//           trigger: '.custom-scroll',
+//           start: 'center center',
+//           direction: 'horizontal',
+//           end: `+=${document.querySelector('.custom-scroll').scrollWidth}px`,
+//           pin: 'body',
+//           scrub: true,
+//           invalidateOnRefresh: true,
+//           ease: "power1.inOut"
+//         },
+//       })
+//     }
+//   }
 
-  make() {
-    this.gsapInit();
-  }
-}
+//   make() {
+//     this.gsapInit();
+//   }
+// }
 
-export default new Scroller();
+// export default new Scroller();
