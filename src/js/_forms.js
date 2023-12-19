bd15a9fb83e8db16f88abab1cd58305d22b74011
Slavalenets95 <V.Lenets@gpsolutions.com>
2023-12-19 12:44:58 +0300

    Fixes

--- src/js/_forms.js
+++ src/js/_forms.js
@@ -8,11 +8,11 @@ class Forms {
     const selectWrappers = document.querySelectorAll('.select-wrapper');
     const body = document.querySelector('body');
 
-    if(selects.length) {
+    if (selects.length) {
       selects.forEach(select => {
         select.addEventListener('change', (evt) => {
-          if(evt.target.value) {
-            evt.target.setAttribute('data-selected' , '');
+          if (evt.target.value) {
+            evt.target.setAttribute('data-selected', '');
           } else {
             evt.target.removeAttribute('data-selected');
           }
@@ -20,20 +20,20 @@ class Forms {
       })
     }
 
-    if(selectWrappers.length) {
+    if (selectWrappers.length) {
       body.addEventListener('click', (evt) => {
         const openedSelectWrapper = document.querySelectorAll('[data-select-open]');
-        
-        if(openedSelectWrapper.length && evt.target.tagName.toLowerCase() !== 'select') {
+
+        if (openedSelectWrapper.length && evt.target.tagName.toLowerCase() !== 'select') {
           openedSelectWrapper.forEach(wrapper => wrapper.removeAttribute('data-select-open'));
         }
       })
       selectWrappers.forEach(wrapper => {
         const select = wrapper.querySelector('select');
 
-        if(select) {
+        if (select) {
           select.addEventListener('click', () => {
-            if(wrapper.hasAttribute('data-select-open')) {
+            if (wrapper.hasAttribute('data-select-open')) {
               wrapper.removeAttribute('data-select-open');
             } else {
               wrapper.setAttribute('data-select-open', '');
